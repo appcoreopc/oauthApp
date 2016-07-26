51ebe09df9734057c294b0454677625288974b41
jeremy <kepung@gmail.com>
2016-07-26 18:06:48 +0800

    oauth with identity server first check in.

---
+++ OAuthApp/OAuthServer/Startup.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using IdentityServer3.Core.Models;
+using System.Collections.Generic;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services.InMemory;
+using Microsoft.Owin.Hosting;
+
+[assembly: OwinStartup(typeof(OAuthServer.Startup))]
+
+namespace OAuthServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            var options = new IdentityServerOptions
+            {
+                Factory = new IdentityServerServiceFactory().
+                    UseInMemoryClients(Clients.Get()).
+                    UseInMemoryScopes(Scopes.Get()).
+                    UseInMemoryUsers(Users.Get()),
+                RequireSsl = false
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+
+
+
+    static class Users
+    {
+        public static List<InMemoryUser> Get()
+        {
+            return new List<InMemoryUser>
+        {
+            new InMemoryUser
+            {
+                Username = "bob",
+                Password = "secret",
+                Subject = "1"
+            },
+            new InMemoryUser
+            {
+                Username = "alice",
+                Password = "secret",
+                Subject = "2"
+            }
+        };
+        }
+    }
+
+    static class Scopes
+    {
+        public static List<Scope> Get()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = "api1"
+                }
+            };
+        }
+    }
+
+    static class Clients
+    {
+        public static List<Client> Get()
+        {
+            return new List<Client>
+        {
+            // no human involved
+                new Client
+                {
+                    ClientName = "Silicon-only Client",
+                    ClientId = "silicon",
+                    Enabled = true,
+                    AccessTokenType = AccessTokenType.Reference,
+
+                    Flow = Flows.ClientCredentials,
+
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("F621F470-9731-4A25-80EF-67A6F7C5F4B8".Sha256())
+                    },
+
+                    AllowedScopes = new List<string>
+                    {
+                        "api1"
+                    }
+                },
+                
+                // human is involved
+                new Client
+                {
+                    ClientName = "Silicon on behalf of Carbon Client",
+                    ClientId = "carbon",
+                    Enabled = true,
+                    AccessTokenType = AccessTokenType.Reference,
+                    Flow = Flows.ResourceOwner,
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("21B5F798-BE55-42BC-8AA8-0025B903DC3B".Sha256())
+                    },
+
+                    AllowedScopes = new List<string>
+                    {
+                        "api1"
+                    }
+                }
+
+            };
+        }
+    }
+}
---
+++ OAuthApp/WebApp/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+
+[assembly: OwinStartup(typeof(WebApp.Startup))]
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServer3.AccessTokenValidation.IdentityServerBearerTokenAuthenticationOptions()
+            {
+                Authority = "http://localhost:5000",
+                ValidationMode = IdentityServer3.AccessTokenValidation.ValidationMode.ValidationEndpoint, 
+                RequiredScopes = new[] { "api1" }
+            });
+
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            config.Filters.Add(new AuthorizeAttribute());
+
+            app.UseWebApi(config);
+        }
+    }
+}
